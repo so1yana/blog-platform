2fe5208951c2f79d703d6ac889a45dd48a541576
so1yana <trapbigest@gmail.com>
2024-02-26 21:54:56 +0300

    added updateUser func

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,4 @@
 const getArticles = async (offset = 0) => {
-    console.log('calling api');
     const result = await fetch(
         `https://blog.kata.academy/api/articles?limit=5&offset=${offset}`,
     ).then((response) => response.json());
@@ -7,10 +6,21 @@ const getArticles = async (offset = 0) => {
 };
 
 export const getArticle = async (slug) => {
-    console.log('calling api slug');
     const result = await fetch(`https://blog.kata.academy/api/articles/${slug}`).then((response) =>
         response.json(),
     );
+    return result;
+};
+
+export const updateUser = async (token, body) => {
+    const result = await fetch('https://blog.kata.academy/api/user', {
+        method: 'PUT',
+        headers: {
+            Authorization: `Token ${token}`,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+    }).then((response) => response.json());
 
     return result;
 };
