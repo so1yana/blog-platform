7117d44c25e577376828df8cf5e653fbef089f32
so1yana <trapbigest@gmail.com>
2024-03-06 14:51:18 +0300

    moved store from index

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,29 +4,52 @@ import {
     createRoutesFromElements,
     createBrowserRouter,
 } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { Tuple, configureStore } from '@reduxjs/toolkit';
+import { thunk } from 'redux-thunk';
 import React from 'react';
-import Layout from '../layout';
+import Layout from '../../pages/layout';
 import BlogList, { blogLoader } from '../blog-list';
 import Article, { articleLoader } from '../article';
-import Login from '../login';
-import Register from '../register';
-import EditProfile from '../edit-profile';
-import EditArticle from '../edit-article';
+import Login from '../../pages/login';
+import Register from '../../pages/register';
+import EditProfile from '../../pages/edit-profile';
 import RequireAuth from '../hoc/RequireAuth';
+import NewArticle from '../../pages/newArticle';
+import reducer from '../../reducers';
+
+const store = configureStore({ reducer, middleware: () => new Tuple(thunk) });
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<Layout />}>
-            <Route path="/:page?" element={<BlogList />} loader={blogLoader} />
+            <Route
+                path="/:page?"
+                element={<BlogList />}
+                loader={(a) => {
+                    a.context = store.getState().userData.token;
+                    return blogLoader(a);
+                }}
+            />
             <Route
                 path="/article/:slug"
                 element={<Article />}
-                loader={articleLoader}
+                loader={(a) => {
+                    a.context = store.getState().userData.token;
+                    return articleLoader(a);
+                }}
                 errorElement={<h2 style={{ marginTop: 96 }}>Not found</h2>}
             />
-            <Route path="/article/:slug/edit" element={<EditArticle />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route
+                path="/article/:slug/edit"
+                element={
+                    <RequireAuth>
+                        <NewArticle />
+                    </RequireAuth>
+                }
+            />
             <Route
                 path="/profile"
                 element={
@@ -35,12 +58,24 @@ const router = createBrowserRouter(
                     </RequireAuth>
                 }
             />
+            <Route
+                path="/new-article"
+                element={
+                    <RequireAuth>
+                        <NewArticle />
+                    </RequireAuth>
+                }
+            />
         </Route>,
     ),
 );
 
 function App() {
-    return <RouterProvider router={router} />;
+    return (
+        <Provider store={store}>
+            <RouterProvider router={router} />
+        </Provider>
+    );
 }
 
 export default App;
