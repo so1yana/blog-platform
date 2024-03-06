56b9feb8ac77c30111ff56f2ff40f48a9972eb21
so1yana <trapbigest@gmail.com>
2024-03-06 14:50:09 +0300

    moved store init to app file

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
-import { Tuple, configureStore } from '@reduxjs/toolkit';
-import { thunk } from 'redux-thunk';
-import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import reducer from './reducers';
 import './index.scss';
 import App from './components/app';
-
-const store = configureStore({ reducer, middleware: () => new Tuple(thunk) });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
+        <App />
     </React.StrictMode>,
 );
