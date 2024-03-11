6b3016ae9959d811bd91bfbaa695395984ef629e
so1yana <trapbigest@gmail.com>
2024-03-11 19:44:13 +0300

    converted to redux-toolkit

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,6 @@
+import { createSlice } from '@reduxjs/toolkit';
+
 const initalState = {
-    amount: 5,
-    page: 1,
-    status: 'ready',
     userData: {
         token: localStorage.getItem('token') || null,
         email: '',
@@ -16,32 +15,25 @@ const defUserData = {
     email: '',
     username: '',
     bio: '',
-    image: null,
+    image: '',
 };
 
-// eslint-disable-next-line default-param-last
-const reducer = (state = initalState, action) => {
-    switch (action.type) {
-        case 'AMOUNT':
-            return { ...state, amount: action.payload };
-        case 'ADD_ARTICLE':
-            return { ...state, article: action.payload };
-        case 'STATUS':
-            return { ...state, status: action.payload };
-        case 'PAGE':
-            return { ...state, page: action.payload };
-        case 'TOKEN':
-            return { ...state, userData: { ...state.userData, token: action.payload } };
-        case 'CLEAR_TOKEN':
-            return { ...state, userData: { ...defUserData, token: null } };
-        case 'SET_USER':
-            return {
-                ...state,
-                userData: { ...state.userData, ...(action.payload?.user || defUserData) },
-            };
-        default:
-            return state;
-    }
-};
+export const reducer = createSlice({
+    name: 'reducer',
+    initialState: initalState,
+    reducers: {
+        token: (state, action) => ({
+            ...state,
+            userData: { ...state.userData, token: action.payload },
+        }),
+        clearToken: (state) => ({ ...state, userData: { ...defUserData, token: null } }),
+        setUser: (state, action) => ({
+            ...state,
+            userData: { ...state.userData, ...(action.payload?.user || defUserData) },
+        }),
+    },
+});
 
-export default reducer;
+export const { amount, addArticle, status, page, token, clearToken, setUser } = reducer.actions;
+
+export default reducer.reducer;
