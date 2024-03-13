4060920dcf577ff489c1e753f21b81e0c9f58a1b
so1yana <trapbigest@gmail.com>
2024-03-13 20:09:16 +0300

    added class support

--- src/components/input/input.js
+++ src/components/input/input.js
@@ -2,20 +2,40 @@ import { forwardRef } from 'react';
 import classes from './input.module.scss';
 
 const Input = forwardRef((props, ref) => {
-    const { type, defaultValue, placeholder, autofocus, onChange, id, name, style, value } = props;
+    const {
+        type,
+        defaultValue,
+        placeholder,
+        autofocus,
+        onChange,
+        id,
+        name,
+        style,
+        value,
+        onBlur,
+        classList,
+    } = props;
+
+    const classesList =
+        (typeof classList === 'string' &&
+            classList?.split(' ')?.map((el) => {
+                return classes[el];
+            })) ||
+        [];
 
     return (
         <input
             id={id}
             ref={ref}
             name={name}
-            className={classes.input}
+            className={`${classes.input} ${classesList.join(' ')}`}
             type={type || 'text'}
             defaultValue={defaultValue}
             value={value}
             placeholder={placeholder}
             autoFocus={autofocus}
             onChange={onChange}
+            onBlurCapture={onBlur}
             style={style}
         />
     );
